d2e0c67d979e89d6a9eb81cceeafc32d6d8baca8
MaryB3 <mariaeugenia.dev@gmail.com>
2018-11-02 19:43:08 -0300

    fix: BookList won't crash with books without image

--- MyReads/reactnd-project-myreads-starter-master/src/BookShelf.js
+++ MyReads/reactnd-project-myreads-starter-master/src/BookShelf.js
@@ -13,10 +13,12 @@ class BookShelf extends Component {
             {books.map((book) => 
                 <li key={book.id} className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ 
-                            width: 128, height: 193, 
-                            backgroundImage: "url(" + book.imageLinks.thumbnail + ")" }}>
-                        </div>
+                    { book.imageLinks && (
+                                    <div className="book-cover" style={{ 
+                                        width: 128, height: 193, 
+                                        backgroundImage: "url(" +  book.imageLinks.thumbnail + ")" }}>
+                                    </div>
+                                )}
                         <div className="book-shelf-changer">
                             <select value={book.shelf} onChange={s => newShelf(book, s.target.value)}>
                             <option value="move" disabled>Move to...</option>
